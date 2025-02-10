81506681a89bad8d9038b3adcab904a73bb56374
CrazyBalls7 <58154829@leerling.agoramaasenpeel.nl>
2025-02-10 14:00:23 +0100

    Ollama API
    AYYAAYAY

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hi")
+import ollama
+Lang_model = 'llama3.2:1b'
+question = input("Question: ")
+
+def ask_ollama(question,model):
+  stream = ollama.chat(
+    model=Lang_model,
+    messages=[{'role': 'user', 'content': question}],
+    stream=True,
+  )
+
+  for chunk in stream:
+    print(chunk['message']['content'], end='', flush=True)
+
+
+
+ask_ollama(question,Lang_model)
